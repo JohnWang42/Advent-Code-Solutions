d25792ca239fb8d7d80b6587d077721b1a75dcaa
Johnny Wang <jwang@allebach.com>
2016-12-06 15:57:40 -0500

    Day 6 2016 solutions

---
+++ 2016/day6-2.py
@@ -0,0 +1,17 @@
+# Day 6
+data = open('input-day6.txt', 'r').read().split('\n')
+msg = []
+for i in range(0, len(data[0])):
+    msg.append({})
+
+for row in data:
+    for i in range(0, len(row)):
+        if row[i] in msg[i]:
+            msg[i][row[i]] += 1
+        else:
+            msg[i][row[i]] = 1
+
+fixedMsg = ''
+for pos in msg:
+    fixedMsg += sorted(pos.iteritems(), key=lambda (k, v): (v, k))[0][0]
+print fixedMsg
